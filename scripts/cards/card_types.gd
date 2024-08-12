58821755d788fbd7dab74fe3d7cd0c03f3f3f5fc
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-08-12 04:56:09 +0300

    card type shown

--- scripts/cards/card_types.gd
+++ scripts/cards/card_types.gd
@@ -1,5 +1,13 @@
 class_name Card extends Node2D
 
 enum Icons {
-    heal
+	heal,
 }
+
+enum Types {
+	effect,
+}
+
+const ICON_COLOR = [
+   Color("df003e") # Icons.heal
+]
--- scripts/cards/main.gd
+++ scripts/cards/main.gd
@@ -5,6 +5,7 @@ const ICONS_PATH = "res://assets/cards/icon/%s.png"
 @export var card_name:String = "Card"
 @export var cost:int = 0
 @export var card_icon:Card.Icons
+@export var card_type:Card.Types
 @export var foreground_texture:Texture2D
 @export var background_texture:Texture2D
 @export var canvas_color:Color = Color.BLACK
@@ -20,9 +21,12 @@ const ICONS_PATH = "res://assets/cards/icon/%s.png"
 @onready var info_box = $Sprite/Canvas/Content/InfoBox
 @onready var separator = $Sprite/Canvas/Content/Separator
 @onready var name_node = $Sprite/Canvas/Content/Textual/CardName
+@onready var type_name = $Sprite/Canvas/Content/TypeName
 
 func configure():
 	name_node.text = card_name
+	type_name.text = Card.Icons.keys()[card_type].capitalize()
+	type_name.label_settings.font_color = Card.ICON_COLOR[card_type]
 	sprite.self_modulate = sprite_color
 	info_box.color = sprite_color
 	separator.default_color = sprite_color
