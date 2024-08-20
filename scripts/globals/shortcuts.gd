61ab8acde647f349a5f287e44e33b8458c91c90a
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-08-20 23:02:54 +0300

    full screen shortcut

---
+++ scripts/globals/shortcuts.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_full_screen"):
+		var is_fullscreen = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED if is_fullscreen else DisplayServer.WINDOW_MODE_FULLSCREEN)
