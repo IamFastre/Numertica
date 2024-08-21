0996cc4bae21252d73a0a17b361df9adc5e54c08
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-08-22 01:56:15 +0300

    card resource + card shadow shader

--- scripts/scenes/card.gd
+++ scripts/scenes/card.gd
@@ -3,25 +3,18 @@ class_name CardNode extends Area2D
 
 const TYPEICONS_PATH = "res://assets/cards/types/%s.png"
 
-@export var card_name:String = "Card"
-@export var cost:int = 0
-@export var card_icon:Card.Icon = Card.Icon.arithmetic
-@export var card_type:Card.Type = Card.Type.effect
-@export var foreground_texture:Texture2D = preload("res://assets/cards/foregrounds/hash.png")
-@export var background_texture:Texture2D = preload("res://assets/cards/backgrounds/dot_grid.png")
-@export var card_style:CardStyle = CardStyle.new()
-@export var parameters:Array[CardParameter]
-
+@export var resource:Card = Card.new()
 
 @onready var name_node:Label = $Sprite/CardName
 @onready var frame:TextureRect = $Sprite/Frame
 @onready var bulk:TextureRect = $Sprite/Bulk
+@onready var bottom_band:TextureRect = $Sprite/BottomBand
 
 @onready var foreground_art:TextureRect = $Sprite/Art/Foreground
 @onready var background_art:TextureRect = $Sprite/Art/Background
 
-@onready var type_icon:TextureRect = $Sprite/Corners/Type
-@onready var cost_icon:TextureRect = $Sprite/Corners/Cost
+@onready var type_icon:TextureRect = $Sprite/Borders/Type
+@onready var cost_icon:TextureRect = $Sprite/Borders/Cost
 
 @onready var band:TextureRect = $Sprite/Band
 @onready var parameters_brackets:Label = $Sprite/Band/Brackets
@@ -53,21 +46,24 @@ func _on_drag_end() -> void:
 # ========================================================================== #
 
 func configure() -> void:
-	name_node.text = card_name
-	frame.self_modulate = card_style.frame_color
-	bulk.self_modulate = card_style.bulk_color
+	name_node.text = resource.name
+	frame.self_modulate = resource.style.frame_color
+	bulk.self_modulate = resource.style.bulk_color
 
-	foreground_art.texture = foreground_texture
-	background_art.texture = background_texture
+	foreground_art.texture = resource.foreground_texture
+	background_art.texture = resource.background_texture
 
-	type_icon.texture = load(TYPEICONS_PATH % Card.Icon.keys()[card_icon])
-	cost_icon.self_modulate = Card.COST_COLOR[cost]
+	type_icon.texture = load(TYPEICONS_PATH % Card.Icon.keys()[resource.type])
+	cost_icon.self_modulate = Card.COST_COLOR[resource.cost]
 
-	if parameters.size() > 0:
+	if resource.parameters.size() > 0:
 		band.visible = true
-		band.self_modulate = card_style.frame_color
-		parameters_brackets.self_modulate = Card.ICON_COLOR[card_icon]
-		parameters_label.text = ', '.join(parameters.map(func(param): return param.get('name')))
-		parameters_label.self_modulate = card_style.accent_color
+		bottom_band.visible = true
+		band.self_modulate = resource.style.frame_color
+		bottom_band.self_modulate = resource.style.frame_color
+		parameters_brackets.self_modulate = Card.ICON_COLOR[resource.type]
+		parameters_label.text = ', '.join(resource.parameters.map(func(param): return param.get('name')))
+		parameters_label.self_modulate = resource.style.accent_color
 	else:
 		band.visible = false
+		bottom_band.visible = false
